6c7d320f75e1e8ca51c1e17e827e82d973e5a866
LexxButOnline <95394760+LexxButOnline@users.noreply.github.com>
2024-04-02 15:13:52 -0500

    Add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 
 
@@ -9,6 +10,7 @@ export default function Page() {
     <main className="text-teal-950 pt-12 bg-[#d9d5c0] bg-background-main bg-repeat body-font pb-10 min-h-screen">
       < About />
       < Contact />
+      < Footer />
     </main >
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <section id="footer">
+            <div className="container mx-auto px-10 mt-10 py-2 text-center bg-teal-800 text-xs text-neutral-200 rounded-xl">
+                Notice: This site is a work in progress.<br />
+                Please pardon any bumps in the road during construction. <br />
+                Thank you!
+            </div>
+        </section >
+    )
+}
